24b50bba5fae5c9f8dd6693b4ee1afa2d2fd8f0c
Numan Safi <numansafi@live.com>
2023-03-03 16:21:47 +0000

    add name for conatiner

--- networks/app.js
+++ networks/app.js
@@ -67,8 +67,9 @@ app.get('/people', async (req, res) => {
   }
 });
 
+
 mongoose.connect(
-  'mongodb://localhost:27017/swfavorites',
+  'mongodb://mongodb/swfavorites',
   { useNewUrlParser: true },
   (err) => {
     if (err) {
